b4a14c66ab66d10a2032fc60f2e7e77175ab1d10
Austin Johnson <technicallyaustin@gmail.com>
2020-02-06 22:39:24 +0000

    Automatically backed up by Learn

--- lib/basic_classes/music_library_controller.rb
+++ lib/basic_classes/music_library_controller.rb
@@ -1,7 +1,9 @@
 class MusicLibraryController
   attr_accessor :path 
   def initialize(path)
-    @path = path 
+    @path = path
+    path = MusicImporter.new(path = './db/mp3s')
+    MusicImporter.import()
   end
   
   
